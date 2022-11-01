c74a082ac9ecd9e72915c444f009ee4c143e1d3c
Sergei Burtsev <spburtsev@proton.me>
2022-11-01 20:11:31 +0200

    dark theme is now default

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	mode: 'jit',
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  mode: 'jit',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        mediumTurquoise: '#00CFC1',
+        brunswickGreen: '#2C4F42',
+        fireBrick: '#BB0A21',
+        erieBlack: '#252627',
+        turquoiseBlue: '#00FFE7',
+      },
+    },
+  },
+  plugins: [],
+};
