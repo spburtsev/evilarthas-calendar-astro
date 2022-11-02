f0bce5655ff5432c2698f57d8db46bf0590a0722
Sergei Burtsev <spburtsev@proton.me>
2022-11-02 13:47:40 +0200

    testValues typo fix

--- tests/no-data-redirect.spec.ts
+++ tests/no-data-redirect.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-const tsetValues = [2014, 2002, 1023, -1];
+const testValues = [2014, 2002, 1023, -1];
 
 test('Redirecting for invalid year param', async ({ page }) => {
-  for (const val of tsetValues) {
+  for (const val of testValues) {
     await page.goto(`http://localhost:3000/calendar/${val}`);
     await expect(page).toHaveTitle('EvilArthas Calendar | No Data');
     await expect(page).toHaveURL('http://localhost:3000/no-data');
